b352d4fc353c3030d2e9b0526e420eafb7c992b6
mathurabargavee <80213592+mathurabargavee@users.noreply.github.com>
2021-03-08 13:38:28 +0530

    Printing the file extenstion
    Your second task now is to write a Python program to accept a filename from the user and print the extension of that.
    Your Input and Output should look something like this
    
    Input the Filename: abc.py The extension of the file is : 'python'

---
+++ TASK 2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+filename = input("Input the Filename: ")
+extension = filename.split(".")
+print ("The extension of the file is : ",repr(extension[-1]))
+
+
+# In[ ]:
+
+
+
+

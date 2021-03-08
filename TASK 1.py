0da2a97200748e5f9b49c2c607900e4493caf0b6
mathurabargavee <80213592+mathurabargavee@users.noreply.github.com>
2021-03-08 12:21:59 +0530

    Add files via upload

---
+++ TASK 1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+r=int(input("Enter radius of circle:"))
+pi=3.141592653589793238
+area=pi*r*r
+print("Area of Circle is",area)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
